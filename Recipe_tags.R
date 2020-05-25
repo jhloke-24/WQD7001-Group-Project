095770de3dc51261d478f76b68b3c69702c007e2
qianhui10 <61973458+qianhui10@users.noreply.github.com>
2020-05-25 11:07:10 +0800

    Update Recipe_tags.R

--- Recipe_tags.R
+++ Recipe_tags.R
@@ -5,10 +5,10 @@ library(splitstackshape)
 
 recipe_data <- read_csv("C:/Users/user/Downloads/RAW_recipes.csv")
 
-#create name and tags data frame and split tags by comma
-recipe_data %<>%
-  select(name, tags) %>%
-  cSplit("tags",",")
+#split tags by comma and drop extra tags
+recipe_data = recipe_data %>%
+  cSplit("tags",",") %>%
+  select(-("tags_07":"tags_73"))
 
 #remove punctuations and extra blank space
 recipe_data <- gsub("[[:punct:][:blank:]]+", " ", as.matrix(recipe_data))
