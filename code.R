e21acec9e836848c020c1160fc0ef213497e0b89
qianhui10 <61973458+qianhui10@users.noreply.github.com>
2020-05-31 12:08:25 +0800

    Update code.R

--- code.R
+++ code.R
@@ -6,7 +6,7 @@ library(shinyalert)
 library(shinythemes)
 library(readr)
 
-testdata <- read_csv("C:/Users/User/Documents/part1.csv")
+testdata <- read_csv("C:/Users/user/Documents/part1.csv")
 testdata <- select(testdata, -X1)
 testdata$config_split <- strsplit(as.character(testdata$ingredients),split = ",")
 testdata <- rowwise(testdata)
@@ -21,34 +21,33 @@ ui <- fluidPage(
   theme= shinytheme("united"),
   themeSelector(),
   navbarPage(
-  "Clear your Fridge",
-  tabPanel("Find your Recipe",
-           sidebarLayout(
-             position = "left",
-             sidebarPanel(width = side_width,
-                          h3("Filter Recipes"),
-                          selectizeInput('e1', '1.Select Ingredients', choices = config, multiple = TRUE),
-                          hr(),       
-                          br(), 
+    "Clear your Fridge",
+    tabPanel("Find your Recipe",
+             sidebarLayout(
+               position = "left",
+               sidebarPanel(width = side_width,
+                            h3("Filter Recipes"),
+                            selectizeInput('e1', '1.Select Ingredients', choices = config, multiple = TRUE),
+                            hr(),       
+                            br(), 
                             selectizeInput('e2', '2. Select Time Range', choices = duration, multiple = FALSE),
                             selectizeInput('e3', '3. Select Calories Level', choices = calorie, multiple = FALSE),
                             selectizeInput('e4', '4. Select Carbohydrates Level', choices = carbohydrate, multiple = FALSE)),   
-#                           selectizeInput('e2', '2. Select Time Range', choices = duration, multiple = FALSE),
-#                           sliderInput('e3','3.Select Calories Level',min=0,max=450000,value=0,step=NULL),
-#                           sliderInput('e4','4.Select Carbohydrates Level',min=0,max=37000,value=0,step=NULL)),
-             mainPanel(
-               width = 12 - side_width,
-               DT::dataTableOutput("mytable")
+               #                           selectizeInput('e2', '2. Select Time Range', choices = duration, multiple = FALSE),
+               #                           sliderInput('e3','3.Select Calories Level',min=0,max=450000,value=0,step=NULL),
+               #                           sliderInput('e4','4.Select Carbohydrates Level',min=0,max=37000,value=0,step=NULL)),
+               mainPanel(
+                 width = 12 - side_width,
+                 DT::dataTableOutput("mytable")
+               )
              )
-           )
-  )
+    )
   )
   
 )
 
 server<-function(input,output){
   output$mytable <- renderDataTable({
-    
     if(isTruthy(input$e1))
     {result <- filter(testdata,all(input$e1 %in% config_split))
     r2<- result %>% ungroup %>% select(Recipe = name,
@@ -66,60 +65,59 @@ server<-function(input,output){
     print(r2)
   }
   ,escape = FALSE,extensions = 'Responsive', selection=list(mode="single", target="row"),
-                                            options = list(pageLength = 10, autoWidth = TRUE,
-                                                           dom  = 'tip',columnDefs = list(list(
-                                                             targets = 1,
-                                                             render = JS(
-                                                               "function(data, type, row, meta) {",
-                                                               "return type === 'display' && data.length > 125 ?",
-                                                               "'<span title=\"' + data + '\">' + data.substr(0, 125) + '...</span>' : data;",
-                                                               "}")
-                                                           ))), callback = JS('table.page(3).draw(false);'),
+  options = list(pageLength = 10, autoWidth = TRUE,
+                 dom  = 'tip',columnDefs = list(list(
+                   targets = 1,
+                   render = JS(
+                     "function(data, type, row, meta) {",
+                     "return type === 'display' && data.length > 125 ?",
+                     "'<span title=\"' + data + '\">' + data.substr(0, 125) + '...</span>' : data;",
+                     "}")
+                 ))), callback = JS('table.page(3).draw(false);'), 
   rownames= FALSE)
   
-
-  
-  
- #extract values from clicked rows 
+  #extract values from clicked rows 
   detail1 <-  renderText({
-              s <- input$mytable_rows_selected
-              paste(testdata[s,1])
-              })
+    filtered <- filter(testdata,all(input$e1 %in% config_split))
+    s <- input$mytable_rows_selected
+    paste(filtered[s, 2])
+  })
   
   detail2 <- renderText({
-              s <- input$mytable_rows_selected
-              paste(testdata[s,4])
-              })
+    filtered <- filter(testdata,all(input$e1 %in% config_split))
+    s <- input$mytable_rows_selected
+    paste(filtered[s,5])
+  })
   
   detail3 <- renderText({
-               s <- input$mytable_rows_selected
-              paste(testdata[s,3])
-              })
+    filtered <- filter(testdata,all(input$e1 %in% config_split))
+    s <- input$mytable_rows_selected
+    paste(filtered[s,4])
+  })
   
   detail4 <- renderText({
-              s <- input$mytable_rows_selected
-              paste(testdata[s,14]," , ",testdata[s,15]," , ",testdata[s,16])
-              })
-              
-  
+    filtered <- filter(testdata,all(input$e1 %in% config_split))
+    s <- input$mytable_rows_selected
+    paste(filtered[s,18]," , ",filtered[s,19]," , ",filtered[s,20])
+  })
   
   output$modal_text <- renderPrint({
-    HTML("<b>Recipe</b>", "<br/>")})
+    HTML("<h2><b>Recipe</b></h2>", "<p></p>")})
   
   output$modal_text1 <- detail1
   
   output$modal_text2 <- renderPrint({
-    HTML("<br/><br/>","<b>Ingredients</b>", "<br/>")})
+    HTML("<hr>", "<h4><b>Ingredients</b></h4>", "<p></p>")})
   
   output$modal_text3 <- detail2
   
   output$modal_text4 <- renderPrint({
-    HTML("<br/><br/>","<b>Steps</b>", "<br/>")})  
-
+    HTML("<p></p>", "<h4><b>Steps</b></h4>", "<p></p>")})  
+  
   output$modal_text5 <- detail3
-
+  
   output$modal_text6 <- renderPrint({
-    HTML("<br/><br/>","<b>Tags</b>", "<br/>")}) 
+    HTML("<hr>", "<h4><b>Tags</b></h4>", "<p></p>")}) 
   
   output$modal_text7 <- detail4
   
