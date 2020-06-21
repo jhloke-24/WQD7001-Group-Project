5e85cb0b2e604248dc896fcdd03935c27e9bd15a
Khairul Hafiz <57560964+hafizaya62@users.noreply.github.com>
2020-06-22 00:40:38 +0800

    Update Shiny Apps.R

--- Shiny Apps.R
+++ Shiny Apps.R
@@ -7,13 +7,17 @@ library(shinythemes)
 library(readr)
 library(lubridate)
 
-testdata <- read_csv("part1_V2.csv")
+#load data
+testdata <- read_csv("Recipe.csv")
+#splitting variables for selection
 testdata$config_split <- strsplit(as.character(testdata$ingredients),split = ",")
 testdata <- rowwise(testdata)
 config <- unique(unlist(strsplit(as.character(testdata$ingredients), ",")))
 duration <- sort(unique(unlist(strsplit(as.character(testdata$g_minutes), ","))))
 calorie <- sort(unique(unlist(strsplit(as.character(testdata$g_calorie), ","))))
 carbohydrate <- sort(unique(unlist(strsplit(as.character(testdata$g_carbohydrate), ","))))
+#create empty matrix to store filtered values
+mat <- matrix(1:9, ncol = 9)
 
 
 side_width = 4
@@ -21,6 +25,7 @@ ui <- fluidPage(
     theme= shinytheme("united"),
     navbarPage(
         "Clear your Fridge",
+        #Tab Page 1
         tabPanel("About",HTML(paste0("This app helps to recommend recipes based on ingredients, preperation time and nutrition values.",
                                      br(),br(),'Instruction:',br(),
                                      "i) Select your ingredients, expected preperation time and desired nutrition level at the drop box on the left.",
@@ -28,6 +33,7 @@ ui <- fluidPage(
                                      "iii) Click on the recipe to prompt for detail steps and informations.",br(),
                                      br(),"Happy cooking!",br(),br())),
                  fluidPage(img(src = 'sushi.jpg', height = '100px', width = '100px'))),
+        #Tab Page 2
         tabPanel("Find your Recipe",
                  sidebarLayout(
                      position = "left",
@@ -37,10 +43,7 @@ ui <- fluidPage(
                                   br(), 
                                   selectizeInput('e2', '2. Select Time Range', choices = duration, multiple = TRUE),
                                   selectizeInput('e3', '3. Select Calories Level', choices = calorie, multiple = TRUE),
-                                  selectizeInput('e4', '4. Select Carbohydrates Level', choices = carbohydrate, multiple = TRUE),   
-                                  #                           selectizeInput('e2', '2. Select Time Range', choices = duration, multiple = FALSE),
-                                  #                           sliderInput('e3','3.Select Calories Level',min=0,max=450000,value=0,step=NULL),
-                                  #                           sliderInput('e4','4.Select Carbohydrates Level',min=0,max=37000,value=0,step=NULL),
+                                  selectizeInput('e4', '4. Select Carbohydrates Level', choices = carbohydrate, multiple = TRUE),
                                   strong("5. Cooking Timer"),
                                   br(),br(),
                                   actionButton('start','Start'),
@@ -60,7 +63,14 @@ ui <- fluidPage(
     
 )
 
+
+
 server<-function(input,output,session){
+    
+    
+    global <- reactiveValues(mat = NULL)
+    
+    #filter selected preferences
     output$mytable <- renderDataTable({
         if(isTruthy(input$e1))
         {result <- testdata %>%
@@ -72,69 +82,54 @@ server<-function(input,output,session){
                                            Ingredient = ingredients,
                                            `Preparation Time` = hours,
                                            Calorie = nutrition_calorie,
-                                           Carbohydrate = nutrition_carbohydrate)
+                                           Carbohydrate = nutrition_carbohydrate,
+                                           Steps = steps,
+                                           Tag1 = tags_01,
+                                           Tag2 = tags_02,
+                                           Tag3 = tags_03)
         }else {
             r2<- testdata %>% ungroup %>% select(Recipe = name,
                                                  Ingredient = ingredients,
                                                  `Preparation Time` = hours,
                                                  Calorie = nutrition_calorie,
-                                                 Carbohydrate = nutrition_carbohydrate)
+                                                 Carbohydrate = nutrition_carbohydrate,
+                                                 Steps = steps,
+                                                 Tag1 = tags_01,
+                                                 Tag2 = tags_02,
+                                                 Tag3 = tags_03)
         }
         print(r2)
+        global$mat <- r2
     }
     ,escape = FALSE,extensions = 'Responsive', selection=list(mode="single", target="row"),
     options = list(pageLength = 10, autoWidth = TRUE,
-                   dom  = 'tip',columnDefs = list(list(
-                       targets = 1,
-                       render = JS(
-                           "function(data, type, row, meta) {",
-                           "return type === 'display' && data.length > 125 ?",
-                           "'<span title=\"' + data + '\">' + data.substr(0, 125) + '...</span>' : data;",
-                           "}")
-                   ))), callback = JS('table.page(3).draw(false);'), 
-    rownames= FALSE)
+                   dom  = 'tip',columnDefs = list(list(visible=FALSE, targets=6:9)),
+    rownames= FALSE))
     
-    #extract values from clicked rows 
+    
+    #extract values from clicked rows for popup
     detail1 <-  renderText({
-        filtered <- testdata %>%
-            filter(all(input$e1 %in% config_split)) %>%
-            filter(all(input$e2 %in% g_minutes)) %>%
-            filter(all(input$e3 %in% g_calorie)) %>%
-            filter(all(input$e4 %in% g_carbohydrate))
         s <- input$mytable_rows_selected
-        paste(filtered[s,2])
+        paste(global$mat[s,1])
     })
     
     detail2 <- renderText({
-        filtered <- testdata %>%
-            filter(all(input$e1 %in% config_split)) %>%
-            filter(all(input$e2 %in% g_minutes)) %>%
-            filter(all(input$e3 %in% g_calorie)) %>%
-            filter(all(input$e4 %in% g_carbohydrate))
         s <- input$mytable_rows_selected
-        paste(filtered[s,5])
+        paste(global$mat[s,2])
     })
     
     detail3 <- renderText({
-        filtered <- testdata %>%
-            filter(all(input$e1 %in% config_split)) %>%
-            filter(all(input$e2 %in% g_minutes)) %>%
-            filter(all(input$e3 %in% g_calorie)) %>%
-            filter(all(input$e4 %in% g_carbohydrate))
         s <- input$mytable_rows_selected
-        paste(filtered[s,4])
+        paste(global$mat[s,6])
     })
     
     detail4 <- renderText({
-        filtered <- testdata %>%
-            filter(all(input$e1 %in% config_split)) %>%
-            filter(all(input$e2 %in% g_minutes)) %>%
-            filter(all(input$e3 %in% g_calorie)) %>%
-            filter(all(input$e4 %in% g_carbohydrate))
         s <- input$mytable_rows_selected
-        paste(filtered[s,18]," , ",filtered[s,19]," , ",filtered[s,20])
+        paste(global$mat[s,7]," , ",global$mat[s,8]," , ",global$mat[s,9])
     })
     
+    
+    #print values for popup
     output$modal_text <- renderPrint({
         HTML("<h2><b>Recipe</b></h2>", "<p></p>")})
     
@@ -155,7 +150,7 @@ server<-function(input,output,session){
     
     output$modal_text7 <- detail4
     
-    
+    #display popup values in sequence
     myModal <- function(failed=FALSE){
         modalDialog(
             htmlOutput('modal_text'),
@@ -174,6 +169,7 @@ server<-function(input,output,session){
         showModal(myModal())
     })
     
+    #Timer
     # Initialize the timer, 10 seconds, not active.
     timer <- reactiveVal(10)
     active <- reactiveVal(FALSE)
